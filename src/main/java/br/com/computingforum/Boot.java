5fce04d7a8c22b8453841f24ce3c799c6097a7e2
brunowall <brunowallison.fernandes@gmail.com>
2017-06-05 01:49:34 -0300

    Adicionando filtros

--- src/main/java/br/com/computingforum/Boot.java
+++ src/main/java/br/com/computingforum/Boot.java
@@ -1,12 +1,16 @@
 package br.com.computingforum;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import br.com.computingforum.filtros.FilterLogin;
 
 @SpringBootApplication
-public class Boot
-{
-
+public class Boot extends WebMvcConfigurerAdapter{
+	
    public static void main(String[] args)
    {
       SpringApplication.run(Boot.class, args);
